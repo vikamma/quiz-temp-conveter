2f5ff0100689380918c3e9ce0f4f5b07f2c74c47
Manav Misra <manavm@visionwebsoft.com>
2022-08-12 05:49:20 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,19 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+const converters = {
+  toCelsius(fahrenheit) {
+    return ((fahrenheit - 32) * 5) / 9;
+  },
+  toFahrenheit(celsius) {
+    return (celsius * 9) / 5 + 32;
+  },
+};
+
+export default function tryConvert(temperature, conversion) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+
+  const output = converters[conversion](input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
